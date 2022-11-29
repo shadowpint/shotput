8328eb07555645dce3be5af779f6a8916f0e203e
Neeraj <neeraj.giri@magicpin.in>
2022-11-30 03:05:57 +0530

    changes

--- shotput/settings.py
+++ shotput/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 SITE_URL = 'stark-scrubland-55374.herokuapp.com/'
 
 # Default primary key field type
